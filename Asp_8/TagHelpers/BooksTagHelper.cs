ef5876247c891a50988f9a4c6fcd0d1bcc6fc57c
R0Y4L <royalb9514@gmail.com>
2023-08-03 12:26:28 +0400

    TagHelper

--- Asp_8/TagHelpers/BooksTagHelper.cs
+++ Asp_8/TagHelpers/BooksTagHelper.cs
@@ -4,12 +4,11 @@ using System.Text;
 
 namespace Asp_8.TagHelpers;
 
-
 [HtmlTargetElement("books")]
 public class BooksTagHelper : TagHelper
 {
     public Book_messageVM? Items { get; set; }
-
+    public int Id { get; set; } = default;
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = "tbody";
@@ -17,20 +16,30 @@ public class BooksTagHelper : TagHelper
 
         foreach (BookViewModel item in Items?.BookViewModels!)
         {
-            sb.Append("<tr>");
-            sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.BookId);
-            sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Name);
-            sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Category);
-            sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Theme);
-            sb.AppendFormat("<td class=\"text-center\"> {0} {1} </td>", item.AuthorName, item.AuthorSurname);
-            sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Press);
-            sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Price);
-            sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Count);
-            sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Description);
-            sb.AppendFormat("<td class=\"text-center\"><ul class=\"list-inline mb-0\"><li class=\"list-inline-item dropdown\"><a class=\"text-muted dropdown-toggle font-size-18 px-2\" href=\"#\" role=\"button\" data-bs-toggle=\"dropdown\" aria-haspopup=\"true\"><i class=\"bx bx-dots-vertical-rounded\"></i></a><div class=\"dropdown-menu dropdown-menu-end\"><a class=\"dropdown-item link-danger\" href=\"/bookStore/delete/{0}\"> Delete </a><a class=\"dropdown-item link-info\" href = \"/bookStore/edit/{0}\"> Edit </a></div></li></ul></td>", item.BookId);
-            sb.Append("</tr>");
+            if (Id == 0)
+                ScreeResult(sb, item);
+            else if (Id != 0 && item.BookId == Id)
+                ScreeResult(sb, item);
         }
 
         output.PreContent.SetHtmlContent(sb.ToString());
+    }
+
+    private StringBuilder ScreeResult(StringBuilder sb, BookViewModel item)
+    {
+        sb.Append("<tr>");
+        sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.BookId);
+        sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Name);
+        sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Category);
+        sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Theme);
+        sb.AppendFormat("<td class=\"text-center\"> {0} {1} </td>", item.AuthorName, item.AuthorSurname);
+        sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Press);
+        sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Price);
+        sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Count);
+        sb.AppendFormat("<td class=\"text-center\"> {0} </td>", item.Description);
+        sb.AppendFormat("<td class=\"text-center\"><ul class=\"list-inline mb-0\"><li class=\"list-inline-item dropdown\"><a class=\"text-muted dropdown-toggle font-size-18 px-2\" href=\"#\" role=\"button\" data-bs-toggle=\"dropdown\" aria-haspopup=\"true\"><i class=\"bx bx-dots-vertical-rounded\"></i></a><div class=\"dropdown-menu dropdown-menu-end\"><a class=\"dropdown-item link-danger\" href=\"/bookStore/delete/{0}\"> Delete </a><a class=\"dropdown-item link-info\" href = \"/bookStore/edit/{0}\"> Edit </a></div></li></ul></td>", item.BookId);
+        sb.Append("</tr>");
+
+        return sb;
     }
 }
